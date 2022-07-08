e7807784bf9f6ca9534f706982f52b48dfd7109f
Luna Liu <lunaliu@buffalo.edu>
2022-07-08 11:43:19 -0400

    from R

---
+++ creating-plots.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+
+#This adds the imm files into the enviornment
+immData <-read.table("all_imm_pCRMeval.txt.bed", header=TRUE, sep= "\t")
+
+allData <-read.table("all_methods_pCRMeval.txt.bed", header=TRUE, sep= "\t")
+
+
+
+
