44f3a1e4cead004bd29cf888bd15e57a76d7f766
Luna Liu <lunaliu@buffalo.edu>
2022-07-12 10:26:34 -0400

    Hmm, working on it

--- creating-plots.R
+++ creating-plots.R
@@ -1,28 +1,59 @@
 library(ggplot2)
 
 #This adds the imm files into the enviornment
-immData <-read.table("all_imm_pCRMeval.txt.bed", header=TRUE, sep= "\t")
+negimmData <-read.table("neg_all_imm_pCRMeval.txt.bed", header=TRUE, sep= "\t")
+origimmData <- read.table("orig_imm_pCRMeval.txt.bed", header=TRUE, sep = "\t")
 
-allData <-read.table("all_methods_pCRMeval.txt.bed", header=TRUE, sep= "\t")
-
-
-#plot = ggplot(data = immData, mapping = aes(x = PercentageRecallREDflyRecovery, y = PercentageTrainingSetSensitivity)) + geom_boxplot()
-
-#boxy = boxplot(~P_value, data = immData, xlab = "X LABEL", ylab = "Y LABEL", main = "QAQ")
-#boxy
+negallData <-read.table("neg_all_methods_pCRMeval.txt.bed", header=TRUE, sep= "\t")
+#origallData <- read.table("orig_negallmethods_pCRMeval.txt.bed", header=TRUE, sep = "\t")
 
 
+#Creating data frames to put the data into one graph
 
+#box = boxplot(x values ~ y values) 
+#box = boxplot(all vs imm ~ actual percentage sensitivity) 
+
+negallsensitivity = c(negallData[8])
+negallrecovery = c(negallData[14])
+
+origallsensitivity = c(origallData[8])
+#origallrecovery = c(origallData[14])
+
+
+negimmsensitivity = c(negimmData[8])
+negimmrecovery = c(negimmData[14])
+
+origimmsensitivity = c(origimmData[8])
+#origimmrecovery = c(origimmData[14])
+
+len <- length(unlist(negallsensitivity))
+
+lineofnegs = rep("Negative", len)
+lineoforig = c(rep("Original", len))
+
+
+
+sensitivity <- append(negimmsensitivity, origimmsensitivity)
+finlen <- append(lineofnegs, lineoforig)
+
+immsensitivityframe <- data.frame(finlen, sensitivity)
+
+print(immsensitivityframe)
 ###### For imm only ###
 
+immsen = boxplot(PercentageTrainingSetSensitivity ~ finlen, immsensitivityframe)
+
+
+
+
 box = boxplot(PercentageTrainingSetSensitivity ~ Method, 
-              immData,
+              negimmData,
               notch = TRUE,
               main = "Percent sensitivity")
 box
 
 boxrec = boxplot(PercentageRedflyRecovered ~ Method, 
-              immData,
+              negimmData,
               notch = TRUE,
               main = "Percent redfly")
 boxrec
@@ -31,14 +62,14 @@ boxrec
 ###### For all data ###
 
 thing2 = boxplot(PercentageTrainingSetSensitivity ~ Method, 
-                 allData,
+                 negallData,
                  notch = TRUE,
                  main = "Percent sensitivity")
 thing2
 
 
 thing2rec = boxplot(PercentageRedflyRecovered ~ Method, 
-                 allData,
+                 negallData,
                  notch = TRUE,
                  main = "Percent redfly")
 thing2rec
