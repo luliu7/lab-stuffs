0524b3e0dc74b54323c159c1dd167efe4ea1bb86
Luna Liu <lunaliu@buffalo.edu>
2022-08-15 14:16:09 -0400

    added significance to number of hits

--- More-graphs.R
+++ More-graphs.R
@@ -1,7 +1,7 @@
 #All plots on one plot... 
 
 
-
+library(ggpubr)
 library(ggplot2)
 ###This program creates a simple box and whisker plot of negative and original for percent sensitivity and redfly recovery (for all methods together, imm only, and for each individual method) 
 
@@ -36,7 +36,7 @@ origallData <- read.table("orig-all-modified_pCRMeval.txt.bed", header=TRUE, sep
   head(mu)
   
   #Makes a histogram to compare number of scrmshaw hits
-  ggplot(cumalldata, aes(x=SCRMs, color=Dset, fill=Dset)) +
+  ggplot(cumalldata, aes(x=SCRMs, color=Dset, fill=Dset)) +stat_compare_means(method = "t.test")+
     geom_histogram(position="identity", alpha=0.5)+
     #geom_histogram(aes(y=..density..), position="identity", alpha=0.5)+
     geom_density(alpha=0.2)+
@@ -46,6 +46,7 @@ origallData <- read.table("orig-all-modified_pCRMeval.txt.bed", header=TRUE, sep
     scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
     labs(title="Comparing Number of Hits Between Negative and Original",x="Number of hits from SCRMshaw", y = "Count")+
     #labs(title="Comparing Number of Hits Between Negative and Original",x="Number of hits from SCRMshaw", y = "Density")+
+    #stat_compare_means(method = "t.test", aes(label =  ..p.format..))+
     theme_classic()
   
   #Makes density plot for compare scrmshaw hits
@@ -54,42 +55,62 @@ origallData <- read.table("orig-all-modified_pCRMeval.txt.bed", header=TRUE, sep
     geom_vline(data=mu, aes(xintercept=grp.mean, color=Dset),
                linetype="dashed")+
     scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
+    
     scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9"))
     
+  
+  ggplot(cumalldata, aes(x = Dset, y = SCRMs, fill = Dset)) +
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("Number of hits from SCRMshaw")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(1200), xmin = c(0.8), xmax = c(2.2),
+      annotation = c("**"), tip_length = 0.01) 
+  
   
   
 #  #Percentage Training Set sensitiity
   
   ggplot(cumalldata, aes(x = Method, y = PercentageTrainingSetSensitivity, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Sensitivity")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Sensitivity")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.92, 1.05, 0.8), xmin = c(0.8, 1.8, 2.8), xmax = c(1.2, 2.2, 3.2),
+      annotation = c("***", "*", "NS"), tip_length = 0.01) 
 
   ggplot(cumalldata, aes(x = "", y = PercentageTrainingSetSensitivity, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Sensitivity")+ xlab("Cumulative")+ stat_compare_means(aes(label =  ..p.signif..))
-  
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Sensitivity")+ xlab("Cumulative")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(1.05), xmin = c(0.8), xmax = c(1.2),annotation = c("***"), tip_length = 0.01) 
   
   #Percent redfly recovery
   
   ggplot(cumalldata, aes(x = Method, y = PercentageRedflyRecovered, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("all Percent Redfly Recovery")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("all Percent Redfly Recovery")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.6, 0.52, 0.5), xmin = c(0.8, 1.8, 2.8), xmax = c(1.2, 2.2, 3.2),
+      annotation = c("****", "****", "****"), tip_length = 0.01) 
+  
   
   ggplot(cumalldata, aes(x = "", y = PercentageRedflyRecovered, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("all Percent Redfly Recovery")+ xlab("Cumulative")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("all Percent Redfly Recovery")+ xlab("Cumulative") + stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.6), xmin = c(0.8), xmax = c(1.2),annotation = c("****"), tip_length = 0.01) 
+  
   
   #Percent Expression Pattern Recall
   
   ggplot(cumalldata, aes(x = Method, y = percentageExpressionPatternRecall, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Recall")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Recall")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.155, 0.18, 0.13), xmin = c(0.8, 1.8, 2.8), xmax = c(1.2, 2.2, 3.2),
+      annotation = c("NS", "*", "NS"), tip_length = 0.01) 
   
   ggplot(cumalldata, aes(x = "", y = percentageExpressionPatternRecall, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Recall")+ xlab("Cumulative")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Recall")+ xlab("Cumulative")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.18), xmin = c(0.8), xmax = c(1.2),annotation = c("NS"), tip_length = 0.01) 
   
   #Percent Expression Pattern Precision
   #par(mfrow=c(1,2))
   ggplot(cumalldata, aes(x = Method, y = percentageExpressionPatternPrecision, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Precision")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Precision")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.6, 0.6, 0.5), xmin = c(0.8, 1.8, 2.8), xmax = c(1.2, 2.2, 3.2),
+      annotation = c("NS", "NS", "NS"), tip_length = 0.01) 
   
   ggplot(cumalldata, aes(x = "", y = percentageExpressionPatternPrecision, fill = Dset)) +
-    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Precision") + xlab("Cumulative")+ stat_compare_means(aes(label =  ..p.signif..))
+    geom_boxplot()+ geom_point(position=position_jitterdodge(), color = "black", size = 0.3) + ggtitle("All Percent Expression Pattern Precision") + xlab("Cumulative")+ stat_compare_means(method = "t.test", aes(label =  ..p.format..))+  geom_signif(
+      y_position = c(0.6), xmin = c(0.8), xmax = c(1.2),annotation = c("*"), tip_length = 0.01) 
   
   
   
@@ -222,10 +243,14 @@ newcumalldata$Dset <- dsetline
 #newcumalldata <-rbind(cumallData, negallData)
 
 
+
+#Same graph, using T test instead
 ggplot(newcumalldata, aes(x=names, y=stuffs, fill=Dset)) + geom_point(position=position_jitterdodge(), size = 0.2) +
   geom_boxplot(alpha = 0.8) + labs(fill = "Type of\nDataset", title="Plot of percentages for different parts",
-                       x ="", y = "Percentage") + theme(axis.text.x = element_text(size = 6)) 
-
+                                   x ="", y = "Percentage") + theme(axis.text.x = element_text(size = 6)) +  geom_signif(
+                                     y_position = c(0.6, 0.2,0.6,1.05), xmin = c(0.8, 1.8, 2.8, 3.8), xmax = c(1.2, 2.2, 3.2, 4.2),
+                                     annotation = c("**", "NS", "****", "***"), tip_length = 0
+                                   ) +stat_compare_means(method = "t.test",aes(label =  ..p.format..))
 
 #Shrinking the dset to 220 units (average of all three methods)
 #for(x in negallData$TsetName)
